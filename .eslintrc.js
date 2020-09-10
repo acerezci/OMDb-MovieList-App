c5d8c88702b4ad1f132c57347e7d716142d5e600
acerezci <alperencerezci@gmail.com>
2020-09-10 14:37:27 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'no-use-before-define': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
+};
---
+++ src/Containers/Home.tsx
@@ -0,0 +1,9 @@
+import React, { memo } from 'react';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+export default memo(Home);
